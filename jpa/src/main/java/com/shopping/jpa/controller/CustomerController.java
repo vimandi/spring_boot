df78e1f07758225d543402e0709824e18936dfc4
vasu <vasu.imandhi@gmail.com>
2024-02-21 23:11:52 +0530

    swagger changes

--- jpa/src/main/java/com/shopping/jpa/controller/CustomerController.java
+++ jpa/src/main/java/com/shopping/jpa/controller/CustomerController.java
@@ -1,14 +1,17 @@
 package com.shopping.jpa.controller;
 
 import com.shopping.jpa.entity.Customer;
-import com.shopping.jpa.repository.CustomerRepository;
-import com.shopping.jpa.repository.ProductRepository;
 import com.shopping.jpa.service.CustomerService;
-import jakarta.websocket.server.PathParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -17,12 +20,31 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    /*@ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content)})*/
+
     @GetMapping("/customers")
+    @Operation(summary = "List all available customers")
+    @ApiResponse(description = "Fetch all customerss", responseCode = "200")
     public List<Customer> customers() {
         List<Customer> customerList = customerService.fetchAllCustomers();
         return customerList;
     }
-
+/*
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })*/
     @PostMapping("/customer")
     public ResponseEntity<Customer> customer(@RequestBody Customer customer) {
         Customer newCustomer = customerService.saveCustomer(customer);
@@ -30,12 +52,19 @@ public class CustomerController {
     }
 
     @PutMapping("/customer/{customerId}")
+    @Operation(summary = "Update given customer details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Customer has been updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Customer is not found", content = @Content)
+    })
     public ResponseEntity<Customer> customer(@RequestBody Customer customer, @PathVariable Integer customerId) {
         customer.setId(customerId);
-        Customer newCustomer =  customerService.saveCustomer(customer);
+        Customer newCustomer = customerService.saveCustomer(customer);
         if (newCustomer != null) {
-           return new ResponseEntity<>(newCustomer, HttpStatus.OK);
-        }else{
+            return new ResponseEntity<>(newCustomer, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -43,11 +72,10 @@ public class CustomerController {
     @DeleteMapping("/customer/{customerId}")
     public ResponseEntity<Void> customer(@PathVariable Integer customerId) {
         boolean deleted = customerService.deleteCustomer(customerId);
-        if(deleted){
+        if (deleted) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
